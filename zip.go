63623517ce25a3a2c40523d6f03d71199d2864e8
LAURENT Louis <louis_laurent@aol.fr>
2023-03-21 01:51:10 +0100

    add multithreads zip and fix diff length issue

--- zip.go
+++ zip.go
@@ -1,7 +1,77 @@
 package iterago
 
+import "sync"
+
 // Zip allow you to merge to array into an array of Pair
 func Zip[T any](first []T, second []T) []Pair[Option[T]] {
+	if len(first) == 0 && len(second) == 0 {
+		return nil
+	}
+
+	if iteragoThreads > 1 {
+		return zipMultithreads(iteragoThreads, first, second)
+	}
+
+	return zip(first, second)
+}
+
+func zipMultithreads[T any](threads uint, first []T, second []T) (result []Pair[Option[T]]) {
+	firstChunks, secondChunks := zipGetChunks(first, second, threads)
+	maxLenght := func(f int, s int) int {
+		if f > s {
+			return f
+		}
+
+		return s
+	}(len(firstChunks), len(secondChunks))
+
+	var wg sync.WaitGroup
+	var mx sync.Mutex
+	for i := 0; i < maxLenght; i += 1 {
+		wg.Add(1)
+
+		go func(index int) {
+			defer wg.Done()
+			var tmp []Pair[Option[T]] = nil
+			if index > len(firstChunks)-1 {
+				tmp = zip(nil, secondChunks[index])
+			}
+			if index > len(secondChunks)-1 {
+				tmp = zip(firstChunks[index], nil)
+			}
+			if tmp == nil {
+				tmp = zip(firstChunks[index], secondChunks[index])
+			}
+			mx.Lock()
+			result = append(result, tmp...)
+			mx.Unlock()
+		}(i)
+	}
+	wg.Wait()
+
+	return
+}
+
+func zipGetChunks[T any](first, second []T, threads uint) ([][]T, [][]T) {
+	if len(first) == 0 {
+		secondSize := getChunkSize(second, threads)
+		return nil, split(second, secondSize)
+	}
+
+	if len(second) == 0 {
+		firstSize := getChunkSize(first, threads)
+		return split(first, firstSize), nil
+	}
+
+	firtSize := getChunkSize(first, threads)
+	firstChunks := split(first, firtSize)
+	secondSize := len(firstChunks[0])
+	secondChunks := chunks(second, uint(secondSize))
+
+	return firstChunks, secondChunks
+}
+
+func zip[T any](first []T, second []T) []Pair[Option[T]] {
 	if len(first) == 0 && len(second) == 0 {
 		return nil
 	}
@@ -14,7 +84,7 @@ func Zip[T any](first []T, second []T) []Pair[Option[T]] {
 					NewOption(second[0]),
 				),
 			},
-			Zip(nil, second[1:])...,
+			zip(nil, second[1:])...,
 		)
 	}
 
@@ -26,7 +96,7 @@ func Zip[T any](first []T, second []T) []Pair[Option[T]] {
 					NewNoneOption[T](),
 				),
 			},
-			Zip(first[1:], nil)...,
+			zip(first[1:], nil)...,
 		)
 	}
 
@@ -37,6 +107,6 @@ func Zip[T any](first []T, second []T) []Pair[Option[T]] {
 				NewOption(second[0]),
 			),
 		},
-		Zip(first[1:], second[1:])...,
+		zip(first[1:], second[1:])...,
 	)
 }
